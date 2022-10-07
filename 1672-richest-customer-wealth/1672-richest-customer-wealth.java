b644e9c4fb6cc5ef3f1b7212c19bd5ec77254470
Aditya Pawar <92213365+Adityapawar11@users.noreply.github.com>
2022-10-07 10:45:15 +0530

    Time: 1 ms (68.44%), Space: 43.7 MB (21.43%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int person=0;person<accounts.length;person++){
+            int rowsum=0;
+            for(int account=0;account<accounts[person].length;account++){
+                rowsum+=accounts[person][account];
+            }
+            if(max<rowsum)
+                max=rowsum;
+        }
+        return max;
+    }
+}
