2d6342e1b3ba4db7e21faf262c334e275e23fa4e
Aditya Pawar <92213365+Adityapawar11@users.noreply.github.com>
2022-11-29 19:12:27 +0530

    Time: 1 ms (99.14%), Space: 42.9 MB (86.63%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,10 +4,11 @@ class Solution {
         for(int  i=0;i<ans.length;i++){
             if(i<nums.length){
                 ans[i]=nums[i];
-                ans[i+nums.length]=nums[i];
                 }
         }
-       
+        for(int i=0;i<nums.length;i++){
+            ans[i+nums.length]=nums[i];
+        }
         return ans;
     }
 }
